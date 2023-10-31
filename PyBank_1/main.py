448b0bb5fd1ffce94a1cf656ed8e8f57af089187
IsmailBilal92 <bilal.ismael92@gmail.com>
2023-10-31 16:30:49 -0400

    python challenge is done

--- PyBank_1/main.py
+++ PyBank_1/main.py
@@ -5,51 +5,73 @@ import csv
 budget_csv = os.path.join("C:\\Users\\Fatima\\Boot-camp\\Third-week\\3rd_challenge\\Starter_Code\\Starter_Code\\PyBank\\Resources\\budget_data.csv")
 analysis = os.path.join("C:\\Users\\Fatima\\Boot-camp\\Third-week\\3rd_challenge\\python-challenge\\PyBank_1\\analysis\\Financial_Analysis.txt")
 
-number_month=0
-total_net=0
-net=0
-list_net=[]
-month_change=[]
-net_change=0
-greater_increase=[]
-greater_decrease=[]
-net_change_list=[]
-average=0
+total_months = 0
+total_pl = 0
+value = 0
+change = 0
+dates = []
+profits = []
 
-with open(budget_csv,'r') as csvfile:
-    csvreader=csv.reader(csvfile,delimiter=',')
-    header=next(csvreader)
+#Opening and reading the CSV file
+with open(budget_csv, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
 
-    for row in csvreader:
-        number_month+=1
+    #Reading the header row
+    csv_header = next(csvreader)
 
-        total_net = total_net + row['Profit/Losses']
-
-        net_change=float(row['Profit/Losses'])-net
-        net=float(row['Profit/Losses'])
-        net_change_list=net_change_list+[net]
-        month_change=[month_change]+[row["Date"]]
-
-        if net_change>greater_increase[1]:
-            greater_increase[1]=net_change
-            greater_increase[0]=row["Date"]
-        
-        if net_change<greater_decrease[1]:
-           greater_decrease[1]=net_change
-           greater_decrease[0]=row["Date"]
-
-    average=sum(net_change_list)/len(net_change_list)
-
-with open(analysis,'w') as text_file:
-    text_file.write("Financial Analysis")
-    text_file.write("...................")   
-    text_file.write("Total month:" %number_month)
-    text_file.write("Totel net"%total_net)
-    text_file.write("Averager"%average)
-    text_file.write("Greatest Increase in net:" % (greater_increase[0], greater_increase[1]))
-    text_file.write("Greatest Decrease in net:" % (greater_decrease[0], greater_decrease[1]))
+    #Reading the first row (so that we track the changes properly)
+    first_row = next(csvreader)
+    total_months += 1
+    total_pl += int(first_row[1])
+    value = int(first_row[1])
     
-       
+    #Going through each row of data after the header & first row 
+    for row in csvreader:
+        # Keeping track of the dates
+        dates.append(row[0])
         
+        # Calculate the change, then add it to list of changes
+        change = int(row[1])-value
+        profits.append(change)
+        value = int(row[1])
         
+        #Total number of months
+        total_months += 1
 
+        #Total net amount of "Profit/Losses over entire period"
+        total_pl = total_pl + int(row[1])
+
+    #Greatest increase in profits
+    greatest_increase = max(profits)
+    greatest_index = profits.index(greatest_increase)
+    greatest_date = dates[greatest_index]
+
+    #Greatest decrease (lowest increase) in profits 
+    greatest_decrease = min(profits)
+    worst_index = profits.index(greatest_decrease)
+    worst_date = dates[worst_index]
+
+    #Average change in "Profit/Losses between months over entire period"
+    avg_change = sum(profits)/len(profits)
+    
+
+#Displaying information
+print("Financial Analysis")
+print("---------------------")
+print(f"Total Months: {str(total_months)}")
+print(f"Total: ${str(total_pl)}")
+print(f"Average Change: ${str(round(avg_change,2))}")
+print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+print(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
+
+#Exporing to .txt file
+output = open("output.txt", "w")
+
+line1 = "Financial Analysis"
+line2 = "---------------------"
+line3 = str(f"Total Months: {str(total_months)}")
+line4 = str(f"Total: ${str(total_pl)}")
+line5 = str(f"Average Change: ${str(round(avg_change,2))}")
+line6 = str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+line7 = str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
+output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7))
